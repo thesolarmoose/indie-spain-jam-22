6f20a7f96d77b805e874986ab3a27c4624a773c9
mnicolas94 <miguelcok27@gmail.com>
2022-09-20 17:00:29 -0400

    FIX: compute movement horizontal limits based on aspect ratio

---
+++ Assets/Scripts/Movement/Controllers/ComputeHorizontalLimits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Utils.Attributes;
+
+namespace Movement.Controllers
+{
+    public class ComputeHorizontalLimits : MonoBehaviour
+    {
+        [SerializeField, AutoProperty(AutoPropertyMode.Scene)]
+        private Camera _camera;
+
+        [SerializeField] private float _halfSubmarineSize;
+        [SerializeField] private LimitHorizontalController _controller;
+
+        private void Start()
+        {
+            ComputeLimits();
+        }
+
+        private void ComputeLimits()
+        {
+            float halfHeight = _camera.orthographicSize;
+            float halfWidth = _camera.aspect * halfHeight;
+            float limit = halfWidth - _halfSubmarineSize;
+
+            _controller.Min = -limit;
+            _controller.Max = limit;
+        }
+    }
+}
--- Assets/Scripts/Movement/Controllers/LimitHorizontalController.cs
+++ Assets/Scripts/Movement/Controllers/LimitHorizontalController.cs
@@ -8,7 +8,19 @@ namespace Movement.Controllers
 
         [SerializeField] private float _min;
         [SerializeField] private float _max;
-        
+
+        public float Min
+        {
+            get => _min;
+            set => _min = value;
+        }
+
+        public float Max
+        {
+            get => _max;
+            set => _max = value;
+        }
+
         public override Vector2 Move(Vector2 previous)
         {
             float x = _target.position.x;
