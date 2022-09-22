0204d842ad62488e54b46260f58eefe75eef5ff3
mnicolas94 <miguelcok27@gmail.com>
2022-09-22 16:26:01 -0400

    FEAT: add id to NarrativeSheet's events

--- Assets/Scripts/NarrativeEvents/Data/NarrativeSheet.cs
+++ Assets/Scripts/NarrativeEvents/Data/NarrativeSheet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace NarrativeEvents.Data
@@ -8,11 +9,31 @@ namespace NarrativeEvents.Data
     public class NarrativeSheet : ScriptableObject
     {
         [SerializeField] private List<Evento> _events;
+
+
+#if UNITY_EDITOR
+        [ContextMenu("Generate events")]
+        private void GenerateEvents()
+        {
+            foreach (var @event in _events)
+            {
+                // obtener id (posición en lista)
+                // obtener dirección de carpeta
+                // ver si carpeta existe
+                // si existe: obtener evento existente
+                // no: crear evento y guardarlo en asset
+                // ver si existe entrada en localization string table
+                // si:
+            }
+        }
+#endif
+        
     }
 
     [Serializable]
     public class Evento
     {
+        [SerializeField, Required] private string _id;
         [SerializeField, TextArea] private string _description;
         [SerializeField] private List<Decision> _decisions;
     }
