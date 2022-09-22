75b9bb8d5bafadbf408cd9fd31ed1828ffa7f363
mnicolas94 <miguelcok27@gmail.com>
2022-09-22 15:27:22 -0400

    FEAT: trigger events with colliders

---
+++ Assets/Scripts/NarrativeEvents/TriggerEventOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NarrativeEvents.UI;
+using UnityEngine;
+using UnityEngine.Events;
+using Utils.Attributes;
+using Utils.Extensions;
+using Event = NarrativeEvents.Data.Event;
+
+namespace NarrativeEvents
+{
+    public class TriggerEventOnCollision : MonoBehaviour
+    {
+        [SerializeField, AutoProperty(AutoPropertyMode.Scene)] private EventDisplayer _eventDisplayer;
+        [SerializeField] private List<Event> _events;
+        [SerializeField] private UnityEvent _onEventEnded;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            var randomEvent = _events.GetRandom();
+            _eventDisplayer.DisplayEvent(randomEvent, () => _onEventEnded.Invoke());
+        }
+    }
+}
