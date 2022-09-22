eae6b44f36eda3244d066d99ea4ba5b8b9be01fb
mnicolas94 <miguelcok27@gmail.com>
2022-09-22 15:42:54 -0400

    FEAT: improve event triggering on collision, and add patrol movement

--- Assets/Scripts/NarrativeEvents/TriggerEventOnCollision.cs
+++ Assets/Scripts/NarrativeEvents/TriggerEventOnCollision.cs
@@ -13,12 +13,19 @@ namespace NarrativeEvents
     {
         [SerializeField, AutoProperty(AutoPropertyMode.Scene)] private EventDisplayer _eventDisplayer;
         [SerializeField] private List<Event> _events;
+        [SerializeField] private LayerMask _collisionMask;
+        
+        [SerializeField] private UnityEvent _onEventStarted;
         [SerializeField] private UnityEvent _onEventEnded;
         
         private void OnTriggerEnter2D(Collider2D other)
         {
-            var randomEvent = _events.GetRandom();
-            _eventDisplayer.DisplayEvent(randomEvent, () => _onEventEnded.Invoke());
+            if (_collisionMask.IsLayerInMask(other.gameObject.layer))
+            {
+                _onEventStarted.Invoke();
+                var randomEvent = _events.GetRandom();
+                _eventDisplayer.DisplayEvent(randomEvent, () => _onEventEnded.Invoke());
+            }
         }
     }
 }
