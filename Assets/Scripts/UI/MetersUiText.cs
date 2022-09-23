24b6657ac43499253c388dab7366a397753a9733
mnicolas94 <miguelcok27@gmail.com>
2022-09-23 17:53:51 -0400

    FEAT: implement UI depth bar

--- Assets/Scripts/UI/MetersUiText.cs
+++ Assets/Scripts/UI/MetersUiText.cs
@@ -8,15 +8,26 @@ namespace UI
     public class MetersUiText : MonoBehaviour, IAtomListener<float>
     {
         [SerializeField] private FloatVariable _meters;
+        [SerializeField] private RectTransform _submarineIcon;
         [SerializeField] private TextMeshProUGUI _text;
 
-        private void Start()
+        private void OnEnable()
         {
             _meters.Changed.RegisterListener(this);
+        }
+        
+        private void OnDisable()
+        {
+            _meters.Changed.UnregisterListener(this);
         }
 
         public void OnEventRaised(float value)
         {
+            const float max = 100;
+            var normalized = value / max;
+            var anchor = new Vector2(0.5f, 1 - normalized);
+            _submarineIcon.anchorMin = anchor;
+            _submarineIcon.anchorMax = anchor;
             _text.text = $"{(int)value}";
         }
     }
