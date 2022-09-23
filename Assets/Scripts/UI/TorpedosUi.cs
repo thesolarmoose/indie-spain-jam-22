35dbd48366ca7dcab6d1d19f8833d90dc8dba1f5
mnicolas94 <miguelcok27@gmail.com>
2022-09-23 17:35:49 -0400

    FEAT: implement torpedos ui

---
+++ Assets/Scripts/UI/TorpedosUi.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace UI
+{
+    public class TorpedosUi : MonoBehaviour
+    {
+        [SerializeField] private IntVariable _torpedosVariable;
+        [SerializeField] private TextMeshProUGUI _torpedosText;
+
+        private void OnEnable()
+        {
+            _torpedosVariable.Changed.Register(OnTorpedosChanged);
+        }
+
+        private void OnDisable()
+        {
+            _torpedosVariable.Changed.Unregister(OnTorpedosChanged);
+        }
+
+        private void OnTorpedosChanged(int torpedos)
+        {
+            _torpedosText.text = $"{_torpedosVariable.Value}";
+        }
+    }
+}
