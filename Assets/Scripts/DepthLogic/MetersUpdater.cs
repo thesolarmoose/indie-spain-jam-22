719ad0b1d542cccd58e34e6a46152d0aef843414
mnicolas94 <miguelcok27@gmail.com>
2022-09-25 08:49:47 -0400

    FEAT: add scale to depth updater. add gradient background

--- Assets/Scripts/DepthLogic/MetersUpdater.cs
+++ Assets/Scripts/DepthLogic/MetersUpdater.cs
@@ -7,10 +7,12 @@ namespace DepthLogic
     {
         [SerializeField] private Transform _target;
         [SerializeField] private FloatVariable _metersVariable;
-
+        [SerializeField] private float _scale;
+        
         private void Update()
         {
             var depth = _target.position.y;
+            depth /= _scale;
             _metersVariable.Value = -depth;
         }
     }
