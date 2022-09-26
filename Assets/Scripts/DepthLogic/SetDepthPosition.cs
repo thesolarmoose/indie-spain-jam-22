18fd0619b901ff26b59eea531b30effe258b30b6
mnicolas94 <miguelcok27@gmail.com>
2022-09-26 09:44:45 -0400

    FEAT: add platforms

---
+++ Assets/Scripts/DepthLogic/SetDepthPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace DepthLogic
+{
+    public class SetDepthPosition : MonoBehaviour
+    {
+        [SerializeField] private float _depth;
+        [SerializeField] private FloatVariable _depthScale;
+
+        private void Start()
+        {
+            var pos = transform.position;
+            pos.y = -_depth * _depthScale.Value;
+            transform.position = pos;
+        }
+    }
+}
